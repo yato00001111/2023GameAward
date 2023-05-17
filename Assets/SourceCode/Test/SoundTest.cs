b9dd7a66320ab0d318b63d19a5d6030f4b168644
MSI\2210083 <korutopi@docomo.ne.jp>
2023-05-17 10:23:06 +0900

    ポーズ処理(コントローラー対応 完成)

--- Assets/SourceCode/Test/SoundTest.cs
+++ Assets/SourceCode/Test/SoundTest.cs
@@ -26,8 +26,7 @@ public class SoundTest : MonoBehaviour
     public bool OddBeat;
 
     public int BeatCount;
-    bool Pena1;
-    bool Pena2;
+    int PenaltyCount;
 
 
     // Start is called before the first frame update
@@ -43,105 +42,66 @@ public class SoundTest : MonoBehaviour
         OddBeat = false;
         EvenBeat = false;
 
+        PenaltyCount = 0;
+
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 60;
+
+        StartCoroutine("BeatPlay");
     }
+
+
+
+
+    private IEnumerator BeatPlay()
+    {
+        while (true)
+        {
+            PlayFlag = true;
+            if (PenaltyFlag)
+            {
+                PlayFlag = false;
+                PenaltyCount++;
+            }
+            yield return new WaitForSeconds(0.15f);
+            PlayFlag = false;
+            yield return new WaitForSeconds(0.3f);
+            PlayFlag = true;
+            if (PenaltyFlag)
+            {
+                PlayFlag = false;
+                PenaltyCount++;
+            }
+            yield return new WaitForSeconds(0.15f);
+        }
+
+    }
+
+
+
 
     // Update is called once per frame
     void FixedUpdate()
     {
+
+        if (!PlayFlag)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                // ペナルティ処理
+                PenaltyFlag = true;
+            }
+        }
+        if (PenaltyCount == 2)
+        {
+            PenaltyFlag = false;
+            PenaltyCount = 0;
+        }
+
         TestText.text = addText.ToString();
 
-
-        // 拍ごとにtrue
-        if (Music.IsJustChangedBeat())
-        {
-            // ゲーム開始フラグ起動
-            FirstBeat = true;
-            BeatCount += 1;
-            //addText += 1;
-        }
-
-        // ゲームが起動したら拍間の時間を計る
-        if (FirstBeat) BeatTimer += Time.deltaTime;
-
-        // 拍の前後0.15フレームは操作を受け付ける
-        if (!PenaltyFlag)
-        {
-            if (BeatTimer > 0.0f && BeatTimer < 0.15f)
-            {
-                PlayFlag = true;
-            }
-            if (BeatTimer > 0.45f && BeatTimer < 0.6f)
-            {
-                PlayFlag = true;
-            }
-            else
-            {
-                PlayFlag = false;
-            }
-        }
-
-        // 次の拍に行ったらタイマーをリセット
-        if (BeatTimer > 0.6f)
-        {
-            BeatTimer = 0.0f;
-        }
-
-        PenaltyMethod();
-
-        // 仮置き 指定フレームだけ操作可能
+        //// 仮置き 指定フレームだけ操作可能
         if (PlayFlag && Input.GetMouseButtonDown(0)) addText += 1;
-    }
-
-
-    void PenaltyMethod()
-    {
-        // もし操作可能フレーム以外で操作が行われたら
-        if (!PlayFlag && Input.GetMouseButtonDown(0))
-        {
-            // ペナルティ処理
-            PenaltyFlag = true;
-        }
-
-        // 現在の拍が奇数が偶数かを判定
-        if (BeatCount % 2 == 0)
-        {
-            EvenBeat = true;
-            OddBeat = false;
-        }
-        else
-        {
-            OddBeat = true;
-            EvenBeat = false;
-        }
-
-
-        // もし拍が偶数の時にペナルティが発生したら
-        if (PenaltyFlag && EvenBeat)
-        {
-            // 偶数用ペナルティフラグを立てる
-            Pena2 = true;
-        }
-        // もし拍が奇数の時にペナルティが発生したら
-        if (PenaltyFlag && OddBeat)
-        {
-            // 奇数用ペナルティフラグを立てる
-            Pena1 = true;
-        }
-
-        // もしペナルティ(偶数)が発生していたら奇数時に解除
-        if (Pena2 && OddBeat)
-        {
-            PenaltyFlag = false;
-            Pena2 = false;
-        }
-        // その逆
-        if (Pena1 && EvenBeat)
-        {
-            PenaltyFlag = false;
-            Pena1 = false;
-        }
     }
 }
 // SceneGameBGM 拍間0.6frame
